851ca789e0e8f5fd43f6975c95c90e923d9bae57
Viet Tu Dao <48443261+DavidDaoTu@users.noreply.github.com>
2021-10-18 21:20:39 +0700

    new merge main to refactor

--- A/hello1.c
+++ A/hello1.c
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-https://stackoverflow.com/questions/68775869/support-for-password-authentication-was-removed-please-use-a-personal-access-to
-=======
 hello, this is added after git reset --hard
 https://stackoverflow.com/questions/68775869/support-for-password-authentication-was-removed-please-use-a-personal-access-to
 
 new lines
->>>>>>> main
+
