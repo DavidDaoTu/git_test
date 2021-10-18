1ac277da86a94a1aa20ba1052dd3603df1d83b14
Viet Tu Dao <48443261+DavidDaoTu@users.noreply.github.com>
2021-10-18 21:01:43 +0700

    continue updating main branch

--- A/hello1.c
+++ A/hello1.c
@@ -1,2 +1,4 @@
 hello, this is added after git reset --hard
 https://stackoverflow.com/questions/68775869/support-for-password-authentication-was-removed-please-use-a-personal-access-to
+
+new lines
